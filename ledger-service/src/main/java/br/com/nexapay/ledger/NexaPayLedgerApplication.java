98f9ec5cb4508a631a763cb939c2e4d22c5a79a9
Jucelio Farias Coelho <juceliocoelho2022@gmail.com>
2026-08-14 20:05:59 -0300

    feat(ledger): bootstrap ledger service

---
+++ ledger-service/src/main/java/br/com/nexapay/ledger/NexaPayLedgerApplication.java
@@ -0,0 +1,12 @@
+package br.com.nexapay.ledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexaPayLedgerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexaPayLedgerApplication.class, args);
+    }
+}
