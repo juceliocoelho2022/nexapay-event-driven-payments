eebef34945fcc3d5a8bc5e96163a63cc3252f21e
Jucelio Farias Coelho <juceliocoelho2022@gmail.com>
2026-08-14 07:14:27 -0300

    feat(account): bootstrap account service

---
+++ account-service/src/main/java/br/com/nexapay/account/NexaPayAccountApplication.java
@@ -0,0 +1,12 @@
+package br.com.nexapay.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexaPayAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexaPayAccountApplication.class, args);
+    }
+}
