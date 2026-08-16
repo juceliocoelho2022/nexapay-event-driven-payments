91d5666a74d1608cc80e01332fd37166c7ae46c0
Jucelio Farias Coelho <juceliocoelho2022@gmail.com>
2026-08-16 16:25:47 -0300

    fix(ledger): load jwt security only for servlet apps

--- ledger-service/src/main/java/br/com/nexapay/ledger/config/LedgerSecurityConfig.java
+++ ledger-service/src/main/java/br/com/nexapay/ledger/config/LedgerSecurityConfig.java
@@ -1,6 +1,7 @@
 package br.com.nexapay.ledger.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -20,6 +21,7 @@ import java.nio.charset.StandardCharsets;
 
 @Configuration
 @EnableMethodSecurity
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
 public class LedgerSecurityConfig {
 
     @Bean
