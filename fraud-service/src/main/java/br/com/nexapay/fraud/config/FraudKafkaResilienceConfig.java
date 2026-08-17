6fe79296394ea10990671e767c8077ddcdb3e6e7
Jucelio Farias Coelho <juceliocoelho2022@gmail.com>
2026-08-17 08:48:28 -0300

    feat: connect fraud retry metrics to error handler

--- fraud-service/src/main/java/br/com/nexapay/fraud/config/FraudKafkaResilienceConfig.java
+++ fraud-service/src/main/java/br/com/nexapay/fraud/config/FraudKafkaResilienceConfig.java
@@ -1,6 +1,7 @@
 package br.com.nexapay.fraud.config;
 
 import br.com.nexapay.fraud.messaging.InvalidFraudEventPayloadException;
+import br.com.nexapay.fraud.observability.KafkaResilienceMetrics;
 import org.apache.kafka.common.TopicPartition;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -17,6 +18,7 @@ public class FraudKafkaResilienceConfig {
     @Bean
     CommonErrorHandler fraudKafkaErrorHandler(
             KafkaTemplate<Object, Object> kafkaTemplate,
+            KafkaResilienceMetrics metrics,
             @Value("${nexapay.kafka.resilience.retry-backoff-ms:1000}") long retryBackoffMs,
             @Value("${nexapay.kafka.resilience.max-retries:2}") long maxRetries
     ) {
@@ -34,6 +36,7 @@ public class FraudKafkaResilienceConfig {
                 new FixedBackOff(retryBackoffMs, maxRetries)
         );
         errorHandler.addNotRetryableExceptions(InvalidFraudEventPayloadException.class);
+        errorHandler.setRetryListeners(metrics);
 
         return errorHandler;
     }
